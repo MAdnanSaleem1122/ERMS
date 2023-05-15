6563d77f0799fafb9b1d2c021428daf476f386c6
AdnanSaleem112233 <adnan.saleem@falconit.com.pk>
2023-05-15 23:15:38 +0500

    Admin ERMS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import Layout from './components/Layout'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from "./components/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import Course from './pages/Course'
-import Profile from './pages/Profile'
-import Login from './pages/Login'
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import AddEmployee from "./pages/AddEmployee";
+import ManageEmployee from "./pages/ManageEmployee";
+import AssignReward from "./pages/AssignReward";
+import AddReward from "./pages/AddReward";
+import ViewReward from "./pages/ViewReward";
+import ManageReward from "./pages/ManageReward";
+import ViewEmployee from "./pages/ViewEmployee";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/dashboard' element={<Dashboard />} />
-                    <Route path='/course' element={<Course />} />
-                    <Route path='/profile' element={<Profile />} />
-                    <Route path='/login' element={<Login />} />
-                </Routes>
-            </Layout>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/addEmployee" element={<AddEmployee />} />
+          <Route path="/manageEmployee" element={<ManageEmployee />} />
+          <Route path="/viewEmployee" element={<ViewEmployee />} />
+          <Route path="/assignReward" element={<AssignReward />} />
+          <Route path="/addReward" element={<AddReward />} />
+          <Route path="/viewReward" element={<ViewReward />} />
+          <Route path="/manageReward" element={<ManageReward />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
